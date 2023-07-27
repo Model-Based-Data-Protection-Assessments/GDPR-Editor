9c3cc507725610417acf2d196f5e711b924b3894
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-07-27 22:50:09 +0200

    Fix flickering between edge lines and arrows

--- src/views.tsx
+++ src/views.tsx
@@ -276,12 +276,14 @@ export class ArrowEdgeView extends PolylineEdgeViewWithGapsOnIntersections {
         for (let i = 1; i < segments.length; i++) {
             const p = segments[i];
             if (i === segments.length - 1) {
-                // Make edge line 10px shorter to make space for the arrow
+                // Make edge line 9px shorter to make space for the arrow
+                // The arrow is 10px long, but we only shorten by 9x to have overlap at the edge between line and arrow.
+                // Otherwise edges would be exactly next to each other which would result in a small gap and flickering if you zoom in enough.
                 const prevP = segments[i - 1];
                 const dx = p.x - prevP.x;
                 const dy = p.y - prevP.y;
                 const length = Math.sqrt(dx * dx + dy * dy);
-                const ratio = (length - 10) / length;
+                const ratio = (length - 9) / length;
                 path += ` L ${prevP.x + dx * ratio},${prevP.y + dy * ratio}`;
             } else {
                 // Lines between points in between are not shortened
