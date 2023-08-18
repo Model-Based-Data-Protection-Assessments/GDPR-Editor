f50cd3c64473ac87f1c6d7021595c739d7524b5f
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-08-19 00:11:21 +0200

    Initial implementation of label type editor ui collapsing

--- src/common/helpUi.tsx
+++ src/common/helpUi.tsx
@@ -18,11 +18,17 @@ export class HelpUI extends AbstractUIExtension {
     protected initializeContents(containerElement: HTMLElement): void {
         containerElement.classList.add("ui-float");
         containerElement.innerHTML = `
-            <div>
-                <p><kbd>CTRL</kbd>+<kbd>Space</kbd>: Command Palette</p>
-                <p><kbd>CTRL</kbd>+<kbd>Z</kbd>: Undo</p>
-                <p><kbd>CTRL</kbd>+<kbd>Shift</kbd>+<kbd>Z</kbd>: Redo</p>
-                <p><kbd>Del</kbd>: Delete selected elements</p>
+            <input type="checkbox" id="accordion-state-help" class="accordion-state" hidden>
+            <label for="accordion-state-help">
+                <div class="accordion-button">Keyboard Shortcuts</div>
+            </label>
+            <div class="accordion-content">
+                <div>
+                    <p><kbd>CTRL</kbd>+<kbd>Space</kbd>: Command Palette</p>
+                    <p><kbd>CTRL</kbd>+<kbd>Z</kbd>: Undo</p>
+                    <p><kbd>CTRL</kbd>+<kbd>Shift</kbd>+<kbd>Z</kbd>: Redo</p>
+                    <p><kbd>Del</kbd>: Delete selected elements</p>
+                </div>
             </div>
         `;
     }
