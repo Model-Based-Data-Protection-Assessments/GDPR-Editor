d4c800076205e488dc1628d0a1ad0d402fab135f
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-09-12 22:09:41 +0200

    Fix help text for default diagram load shortcut

--- src/common/helpUi.tsx
+++ src/common/helpUi.tsx
@@ -32,7 +32,7 @@ export class HelpUI extends AbstractUIExtension {
                     <p><kbd>Del</kbd>: Delete selected elements</p>
                     <p><kbd>T</kbd>: Toggle Label Type Edit UI</p>
                     <p><kbd>CTRL</kbd>+<kbd>O</kbd>: Load diagram from json</p>
-                    <p><kbd>CTRL</kbd>+<kbd>Alt</kbd>+<kbd>O</kbd>: Open default diagram</p>
+                    <p><kbd>CTRL</kbd>+<kbd>Shift</kbd>+<kbd>O</kbd>: Open default diagram</p>
                     <p><kbd>CTRL</kbd>+<kbd>S</kbd>: Save diagram to json</p>
                     <p><kbd>CTRL</kbd>+<kbd>L</kbd>: Automatically layout diagram</p>
                 </div>
