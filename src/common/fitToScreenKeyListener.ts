19ac0c8ab07c41987e356ae3993e6e123cb87e2d
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-11-02 19:58:17 +0100

    Only fit the root nodes to the screen instead of all elements

--- src/common/fitToScreenKeyListener.ts
+++ src/common/fitToScreenKeyListener.ts
@@ -1,7 +1,7 @@
 import { KeyListener, SModelElementImpl } from "sprotty";
-import { Action, CenterAction, FitToScreenAction } from "sprotty-protocol";
+import { Action, CenterAction } from "sprotty-protocol";
 import { matchesKeystroke } from "sprotty/lib/utils/keyboard";
-import { FIT_TO_SCREEN_PADDING } from "../utils";
+import { createDefaultFitToScreenAction } from "../utils";
 
 /**
  * Key listener that fits the diagram to the screen when pressing Ctrl+Shift+F
@@ -11,17 +11,13 @@ import { FIT_TO_SCREEN_PADDING } from "../utils";
  * does not allow setting a padding.
  */
 export class FitToScreenKeyListener extends KeyListener {
-    override keyDown(_element: SModelElementImpl, event: KeyboardEvent): Action[] {
+    override keyDown(element: SModelElementImpl, event: KeyboardEvent): Action[] {
         if (matchesKeystroke(event, "KeyC", "ctrlCmd", "shift")) {
             return [CenterAction.create([])];
         }
 
         if (matchesKeystroke(event, "KeyF", "ctrlCmd", "shift")) {
-            return [
-                FitToScreenAction.create([], {
-                    padding: FIT_TO_SCREEN_PADDING,
-                }),
-            ];
+            return [createDefaultFitToScreenAction(element.root)];
         }
 
         return [];
--- src/features/autoLayout/keyListener.ts
+++ src/features/autoLayout/keyListener.ts
@@ -1,21 +1,18 @@
 import { CommitModelAction, KeyListener, SModelElementImpl } from "sprotty";
 import { matchesKeystroke } from "sprotty/lib/utils/keyboard";
-import { Action, FitToScreenAction } from "sprotty-protocol";
+import { Action } from "sprotty-protocol";
 import { LayoutModelAction } from "./command";
-import { FIT_TO_SCREEN_PADDING } from "../../utils";
+import { createDefaultFitToScreenAction } from "../../utils";
 
 export class AutoLayoutKeyListener extends KeyListener {
-    keyDown(_element: SModelElementImpl, event: KeyboardEvent): Action[] {
+    keyDown(element: SModelElementImpl, event: KeyboardEvent): Action[] {
         if (matchesKeystroke(event, "KeyL", "ctrlCmd")) {
             event.preventDefault();
 
             return [
                 LayoutModelAction.create(),
                 CommitModelAction.create(),
-                FitToScreenAction.create(
-                    [], // empty elementIds means fit the whole diagram
-                    { padding: FIT_TO_SCREEN_PADDING },
-                ),
+                createDefaultFitToScreenAction(element.root),
             ];
         }
 
