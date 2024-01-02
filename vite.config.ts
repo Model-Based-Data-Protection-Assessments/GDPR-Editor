80376eaedcf23f8f4621986ba8debdfd8abce23c
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2024-01-02 23:23:39 +0100

    Split monaco into its own js bundle

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,18 @@ export default defineConfig({
     // it is located in a subdirectory/subbase and we need relative paths.
     // The below setting tells vite to use relative paths everywhere.
     base: "",
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Split monaco editor into a separate chunk.
+                    // This improves the loading time of the editor
+                    // as the JS of sprotty/the application can be already parsed
+                    // while the JS of monaco, which is only needed later on user-interaction
+                    // anyway, is still loading.
+                    "monaco-editor": ["monaco-editor"],
+                },
+            },
+        },
+    },
 });
